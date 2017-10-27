22a67ffd500f327042366b2060e142de67858130
Rahul Sawant <Rahul.Sawant@salebuild.com>
2017-10-27 14:44:06 -0400

    Added keys file for more security

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,10 +1,11 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
+const keys = require('./keys');
 
 passport.use(
     new GoogleStrategy({
-        clientID = '',
-        clientSecret = ''
+        clientID = keys.google.clientID,
+        clientSecret = keys.google.clientSecret
     }), () => {
 
     }
